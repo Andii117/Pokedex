7fe216a3315b5a43034afc9e894a56cc4993e1ac
Harold jara <andresjara640@gmail.com>
2021-03-03 16:33:40 -0500

    first commit

---
+++ src/components/form.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+class form extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name : "",
+            img : "#"
+        }
+
+    }
+    /*
+        function : pokeAPI
+        Description: Obtiene la información de la pokeapi
+    */
+    pokeAPI = async () =>{
+        let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.name}`);
+        //let res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+        let data = await res.json();
+
+        //console.log(data.sprites.front_default);
+        this.setState({
+            img: data.sprites.front_default
+        });
+    }
+
+    handleName= event =>{
+        this.setState({
+            name: event.target.value
+        });
+    }
+
+    handleSubmit = event =>{
+        var sub = this.state.name;
+        console.log(sub);
+        this.pokeAPI();
+        //Evento que permite que no se recargue la pagina
+        event.preventDefault();
+    }
+
+    render(){
+      return ( 
+        <div className="card text-center">
+            <div className="card-header">
+                POKEDEX Pokémon
+            </div>
+            <div className="card-body">
+                <h5 className="card-title">Consulte su pokémon</h5>
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>Ingrese el nombre del pokémon:</label><br />
+                        <div className="input-group input-group-sm mb-3">
+                        <span className="input-group-text" id="inputGroup-sizing-sm">Pokémon:</span>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            placeholder = "ingrese el nombre del pokémon"
+                            value={this.state.name}
+                            onChange={this.handleName}
+                            /><br/><br/>
+                        </div>
+                    <button type="submit" href="#" className="btn btn-primary">Buscar el pokémon</button>
+                    <br/><br/>
+                    <label>Imagen frontal</label><br/>
+                    <img src={this.state.img} className="img-thumbnail" alt="..."></img>
+                </form>
+            </div>
+            <div className="card-footer text-muted">
+                PokeAPI
+            </div>
+        </div>
+       );
+    }
+ }
+ 
+ 
+ export default form;
