5a5df3386b79eecc258efcbd4133ac85292df37d
Harold jara <andresjara640@gmail.com>
2021-03-03 18:09:03 -0500

    Actualización con tipo y especie

--- src/components/form.js
+++ src/components/form.js
@@ -8,7 +8,10 @@ class form extends React.Component{
         this.state = {
             name : "",
             img_male : "#",
-            img_shiny: "#"
+            img_shiny: "#",
+            species: "",
+            type: "",
+            loading: true
         }
 
     }
@@ -21,10 +24,13 @@ class form extends React.Component{
         //let res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
         let data = await res.json();
 
-        console.log(data.sprites);
+        console.log(data);
         this.setState({
             img_male: data.sprites.front_default,
-            img_shiny: data.sprites.front_shiny
+            img_shiny: data.sprites.front_shiny,
+            species: data.species.name,
+            type: data.types[0].type.name,
+            loading: true
         });
     }
 
@@ -36,8 +42,13 @@ class form extends React.Component{
 
     handleSubmit = event =>{
         var sub = this.state.name;
+        this.setState({
+            loading: false
+        });
         //console.log(sub);
         this.pokeAPI();
+
+       
         //Evento que permite que no se recargue la pagina
         event.preventDefault();
     }
@@ -54,22 +65,41 @@ class form extends React.Component{
                 <form onSubmit={this.handleSubmit}>
                     <label>Ingrese el nombre del pokémon:</label><br />
                         <div className="input-group input-group-sm mb-3">
-                        <span className="input-group-text" id="inputGroup-sizing-sm">Pokémon:</span>
-                        <input 
-                            type="text" 
-                            className="form-control" 
-                            placeholder = "ingrese el nombre del pokémon"
-                            value={this.state.name}
-                            onChange={this.handleName}
-                            /><br/><br/>
+                            <span className="input-group-text" id="inputGroup-sizing-sm">Pokémon:</span>
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                placeholder = "ingrese el nombre del pokémon"
+                                value={this.state.name}
+                                onChange={this.handleName}
+                                /><br/><br/>
                         </div>
                     <button type="submit" href="#" className="btn btn-primary">Buscar el pokémon</button>
+                    
                     <br/><br/>
                     <label>Pokémon normal</label><br/>
                     <img src={this.state.img_male} className="img-thumbnail" alt="..."></img><br/>
                     <label>Pokémon Shiny</label><br/>
                     <img src={this.state.img_shiny} className="img-thumbnail" alt="..."></img>
-                
+
+                    <div className="input-group input-group-sm mb-3">
+                            <span className="input-group-text" id="inputGroup-sizing-sm">type:</span>
+                            <input 
+                               className="form-control" 
+                               type="text" 
+                               placeholder={this.state.type} 
+                               aria-label="Disabled input example" 
+                               disabled readOnly  />
+                    </div>
+                    <div className="input-group input-group-sm mb-3">
+                            <span className="input-group-text" id="inputGroup-sizing-sm">Specie:</span>
+                            <input 
+                               className="form-control" 
+                               type="text" 
+                               placeholder={this.state.species}
+                               aria-label="Disabled input example" 
+                               disabled readOnly  />
+                    </div>
                 </form>
             </div>
             <div className="card-footer text-muted">
